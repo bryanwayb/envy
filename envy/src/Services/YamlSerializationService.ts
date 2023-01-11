dd6314eae9a9946b587ce0f7625f602216618c8b
Bryan Way <bryanwayb@gmail.com>
2023-01-11 05:17:21 -0600

    Updated schema and added valid !import example

--- envy/src/Services/YamlSerializationService.ts
+++ envy/src/Services/YamlSerializationService.ts
@@ -2,7 +2,7 @@ import Container, { Service } from 'typedi';
 import { load, Type as YamlType, DEFAULT_SCHEMA, Schema } from 'js-yaml';
 import { readFile } from 'fs/promises';
 import { cwd as getCurrentWorkingDirectory } from 'process';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
 import LoggerService from './LoggerService';
 import { readFileSync } from 'fs';
 
@@ -10,41 +10,51 @@ import { readFileSync } from 'fs';
 export default class YamlSerializationService {
     protected readonly _logger = Container.get(LoggerService).ScopeByName(this.constructor.name);
 
-    private _importYamlType: YamlType = new YamlType('!import', {
-        kind: 'scalar',
-        resolve: data => {
-            return (data !== null || data !== undefined) && data.length > 0;
-        },
-        construct: path => {
-            this._logger.LogTrace(`processing !import with: ${path}`);
-
-            const currentDirectory = getCurrentWorkingDirectory();
-            const resolvedFilePath = resolve(currentDirectory, path);
-
-            this._logger.LogTrace(`attempting to load YAML file at ${resolvedFilePath}`);
-
-            try {
-                const dataBuffer = readFileSync(resolvedFilePath, 'utf8');
-                const data = dataBuffer.toString();
-
-                return load(data, {
-                    schema: this._configurationSchema
-                });
-            }
-            catch (ex) {
-                throw new Error(`Error while attempting to !import YAML file ${path}.\n${ex}`);
-            }
-        }
-    });
-
-    private _configurationSchema: Schema = DEFAULT_SCHEMA.extend([
-        this._importYamlType
-    ]);
-
-    LoadYaml<T>(type: { new(data: any): T; }, input: string): T {
+    LoadYaml<T>(type: { new(data: any): T; }, input: string, referenceFilePath: string = null): T {
         this._logger.LogTrace(`parsing YAML: ${input}`);
+
+        let configurationSchema: Schema = null;
+        const importYamlType: YamlType = new YamlType('!import', {
+            kind: 'scalar',
+            resolve: data => {
+                return (data !== null || data !== undefined) && data.length > 0;
+            },
+            construct: path => {
+                this._logger.LogTrace(`processing !import with: ${path}`);
+
+                let baseDirectory: string;
+                if (referenceFilePath) {
+                    baseDirectory = dirname(referenceFilePath);
+                }
+                else {
+                    baseDirectory = getCurrentWorkingDirectory();
+                }
+                const resolvedFilePath = resolve(baseDirectory, path);
+
+                this._logger.LogTrace(`attempting to sync load YAML file at ${resolvedFilePath}`);
+
+                try {
+                    const dataBuffer = readFileSync(resolvedFilePath, 'utf8');
+                    const data = dataBuffer.toString();
+
+                    this._logger.LogTrace(`parsing YAML: ${data}`);
+
+                    return load(data, {
+                        schema: configurationSchema
+                    });
+                }
+                catch (ex) {
+                    throw new Error(`Error while attempting to !import YAML file ${path}.\n${ex}`);
+                }
+            }
+        });
+
+        configurationSchema = DEFAULT_SCHEMA.extend([
+            importYamlType
+        ]);
+
         const data = load(input, {
-            schema: this._configurationSchema
+            schema: configurationSchema
         });
         return new type(data);
     }
@@ -58,6 +68,6 @@ export default class YamlSerializationService {
         const dataBuffer = await readFile(resolvedFilePath, 'utf8');
         const data = dataBuffer.toString();
 
-        return this.LoadYaml<T>(type, data);
+        return this.LoadYaml<T>(type, data, resolvedFilePath);
     }
 };
