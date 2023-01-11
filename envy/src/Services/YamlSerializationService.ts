bd7b1fafa9c6c3c48796f0d56e8ca86a4726bff2
Bryan Way <bryanwayb@gmail.com>
2023-01-10 21:44:10 -0600

    Renamed project to envy and started work for Yaml tags

--- envy/src/Services/YamlSerializationService.ts
+++ envy/src/Services/YamlSerializationService.ts
@@ -1,13 +1,33 @@
 import { Service } from 'typedi';
-import { load } from 'js-yaml';
+import { load, Type as YamlType, DEFAULT_SCHEMA } from 'js-yaml';
 import { readFile } from 'fs/promises';
 import { cwd as getCurrentWorkingDirectory } from 'process';
 import { resolve } from 'path';
 
+let ConfigurationSchema;
+
+const ImportYamlType = new YamlType('!import', {
+    kind: 'scalar',
+    resolve: data => {
+        return (data !== null || data !== undefined) && data.length > 0;
+    },
+    construct: data => {
+        return load(data, {
+            schema: ConfigurationSchema
+        });
+    }
+});
+
+ConfigurationSchema = DEFAULT_SCHEMA.extend([
+    ImportYamlType
+]);
+
 @Service()
 export default class YamlSerializationService {
     LoadYaml<T>(type: { new(data: any): T; }, input: string): T {
-        const data = load(input);
+        const data = load(input, {
+            schema: ConfigurationSchema
+        });
         const instance = new type(data);
         return instance;
     }
